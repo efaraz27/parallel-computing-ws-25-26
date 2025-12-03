a0e14482e10aab2bb96b2fe929ba711dcbbeba4d
Ehtesham Faraz <efaraz27@gmail.com>
2025-12-03 12:24:30 +0100

    ex5.3

---
+++ pi_experiment.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+####### Mail Notify / Job Name / Comment #######
+#SBATCH --job-name="pi_${VARIANT}_${THREADS}_${CHUNK}"
+
+####### Partition #######
+#SBATCH --partition=all
+
+####### Resources #######
+#SBATCH --time=0-00:05:00
+
+####### Node Info #######
+#SBATCH --exclusive
+#SBATCH --nodes=1
+
+####### Output #######
+#SBATCH --output=/home/fd0001542/out/pi_%x.%j.out
+#SBATCH --error=/home/fd0001542/out/pi_%x.%j.err
+
+cd /home/fd0001542/parl_comp/ex5
+
+# Parameters passed via sbatch --export
+variant=${VARIANT:-seq}
+threads=${THREADS:-1}
+chunk=${CHUNK:-0}
+
+echo "Running experiment: variant=${variant} threads=${threads} chunk=${chunk}"
+./pi_par_loop "${variant}" "${threads}" "${chunk}"
+
+
