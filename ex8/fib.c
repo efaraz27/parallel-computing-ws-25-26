e3325dfeb6e189280e772635d4df70a49c36b843
Ehtesham Faraz <efaraz27@gmail.com>
2025-12-10 12:31:36 +0100

    ex8.1

--- ex8/fib.c
+++ ex8/fib.c
@@ -4,10 +4,10 @@
 
 #define DEFAULT_CUTOFF 20
 
-static long long fib_seq(int n) {
+static long long fib_seq(long long n) {
     long long a = 0;
     long long b = 1;
-    for (int i = 0; i < n; ++i) {
+    for (long long i = 0; i < n; ++i) {
         long long tmp = a + b;
         a = b;
         b = tmp;
@@ -15,7 +15,7 @@ static long long fib_seq(int n) {
     return a;
 }
 
-static long long fib_task(int n, int cutoff) {
+static long long fib_task(long long n, long long cutoff) {
     if (n < 2) {
         return n;
     }
@@ -36,7 +36,7 @@ static long long fib_task(int n, int cutoff) {
     return x + y;
 }
 
-static long long fib_parallel(int n, int cutoff) {
+static long long fib_parallel(long long n, long long cutoff) {
     long long result = 0;
 #pragma omp parallel
     {
@@ -46,21 +46,21 @@ static long long fib_parallel(int n, int cutoff) {
     return result;
 }
 
-static int verify_small_values(int cutoff) {
+static long long verify_small_values(long long cutoff) {
     long long expected = 0;
     long long got = 0;
-    for (int i = 0; i <= 10; ++i) {
+    for (long long i = 0; i <= 10; ++i) {
         expected = fib_seq(i);
         got = fib_parallel(i, cutoff);
-        printf("n=%d: expected %lld, got %lld\n", i, expected, got);
+        printf("n=%lld: expected %lld, got %lld\n", i, expected, got);
 
         if (expected != got) {
-            fprintf(stderr, "Mismatch at n=%d: expected %lld, got %lld\n", i,
+            fprintf(stderr, "Mismatch at n=%lld: expected %lld, got %lld\n", i,
                     expected, got);
-            return 0;
+            return -1;
         }
     }
-    return 1;
+    return 0;
 }
 
 int main(int argc, char **argv) {
@@ -71,14 +71,14 @@ int main(int argc, char **argv) {
         n = atoi(argv[1]);
         if (n < 0) {
             fprintf(stderr, "n must be non-negative\n");
-            return 1;
+            return -1;
         }
     }
     if (argc > 2) {
         cutoff = atoi(argv[2]);
         if (cutoff < 0) {
             fprintf(stderr, "cutoff must be non-negative\n");
-            return 1;
+            return -1;
         }
     }
 
